bf947f274960b212316c23591353a2ac1fa3ce4b
Alex <rubanovv.alex@gmail.com>
2023-04-21 01:38:52 +0200

    added Navigation and Footer components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Footer from '@/features/common/Footer/Footer'
 import Navigation from '@/features/common/modules/Navigation'
 
 export default function Home () {
   return (
     <>
       <Navigation />
+      <Footer />
     </>
   )
 }
